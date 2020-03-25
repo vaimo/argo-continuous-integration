d489af8b381f34ec91c782e3d0ea0ddd51e9df51
Pierre Dahlström <pierre.dahlstrom@vaimo.com>
2020-03-25 14:36:21 +0100

    Add repository owner check

--- webhook/main.go
+++ webhook/main.go
@@ -1,51 +1,58 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "io/ioutil"
-    "os/exec"
-    "strconv"
-    "strings"
-    "time"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/buger/jsonparser"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    gin.DisableConsoleColor()
-    gin.SetMode(gin.ReleaseMode)
+	gin.DisableConsoleColor()
+	gin.SetMode(gin.ReleaseMode)
 
-    r := gin.Default()
+	r := gin.Default()
 
-    r.POST("/", func(c *gin.Context) {
-        c.JSON(202, gin.H{})
+	r.POST("/", func(c *gin.Context) {
+		c.JSON(202, gin.H{})
 
-        buf := new(bytes.Buffer)
-        _, _ = buf.ReadFrom(c.Request.Body)
-        
+		buf := new(bytes.Buffer)
+		_, _ = buf.ReadFrom(c.Request.Body)
+
 		gitRepo, _ := jsonparser.GetString(buf.Bytes(), "repository", "full_name")
 		gitRevision, _ := jsonparser.GetString(buf.Bytes(), "push", "changes", "[0]", "new", "name")
-        
-        gitRepoName := strings.Split(gitRepo, "/")[1]
+		owner, _ := jsonparser.GetString(buf.Bytes(), "repository", "owner", "username")
+
+		if owner != os.Getenv("OWNER") {
+			fmt.Printf("Owner not allowed. owner from webhook=%q,owner from environment variable=%q\n", owner, os.Getenv("OWNER"))
+			return
+		}
+
+		gitRepoName := strings.Split(gitRepo, "/")[1]
 		fullGitRepo := "https://bitbucket.org/" + gitRepo + ".git"
-        timestamp := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+		timestamp := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
 
-        argoFilename := "argo" + timestamp + ".yml"
+		argoFilename := "argo" + timestamp + ".yml"
 
-        input, _ := ioutil.ReadFile("argo.yml")
-        temp := bytes.Replace(input, []byte("<git_repo_name>"), []byte(gitRepoName), -1)
-        temp2 := bytes.Replace(temp, []byte("<git_repo_full>"), []byte(fullGitRepo), -1)
-        output := bytes.Replace(temp2, []byte("<git_revision>"), []byte(gitRevision), -1)
-        _ = ioutil.WriteFile(argoFilename, output, 0666)
+		input, _ := ioutil.ReadFile("argo.yml")
+		temp := bytes.Replace(input, []byte("<git_repo_name>"), []byte(gitRepoName), -1)
+		temp2 := bytes.Replace(temp, []byte("<git_repo_full>"), []byte(fullGitRepo), -1)
+		output := bytes.Replace(temp2, []byte("<git_revision>"), []byte(gitRevision), -1)
+		_ = ioutil.WriteFile(argoFilename, output, 0666)
 
 		commandOutput, err := exec.Command("sh", "-c", "./argo submit "+argoFilename).CombinedOutput()
-        if err != nil {
-            fmt.Printf("Accepted webhook request, did NOT start Argo workflow: git_repo=%q,git_revision=%q, because of: %q\n", gitRepo, gitRevision, string(err.Error()))
-        } else {
-            fmt.Printf("Accepted webhook request, started Argo workflow: git_repo=%q,git_revision=%q, with message: %q\n", gitRepo, gitRevision, string(commandOutput))
-        }
-    })
-    _ = r.Run(":3000")
+		if err != nil {
+			fmt.Printf("Accepted webhook request, did NOT start Argo workflow: git_repo=%q,git_revision=%q, because of: %q\n", gitRepo, gitRevision, string(err.Error()))
+		} else {
+			fmt.Printf("Accepted webhook request, started Argo workflow: git_repo=%q,git_revision=%q, with message: %q\n", gitRepo, gitRevision, string(commandOutput))
+		}
+	})
+	_ = r.Run(":3000")
 }
